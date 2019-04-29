116e93e8b421e9732687c9a78ad9c45186dba173
paulotijero <paulotijero@gmail.com>
2019-04-29 18:24:53 -0500

    Add routes and config devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV["FACEBOOK_SECRET"], token_params: { parse: :json }
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -296,6 +298,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV["FACEBOOK_SECRET"], token_params: { parse: :json }
-  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], token_params: { parse: :json }
 end
