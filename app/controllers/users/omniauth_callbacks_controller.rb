e18f881af120b68adbc121435572726b8b9fe57d
paulotijero <paulotijero@gmail.com>
2019-04-29 17:58:16 -0500

    Fix omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,27 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: "Facebook")
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
+  end
+
+  def github
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user
+      sign_in_and_redirect @user
+      set_flash_message(:notice, :success, kind: 'GitHub')
+    else
+      session['devise.github_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
