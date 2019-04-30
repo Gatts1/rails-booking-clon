aa3218be96cb3da1e29c2880cd49b246b708eaba
paulotijero <paulotijero@gmail.com>
2019-04-29 20:37:08 -0500

    delete migrate

--- db/migrate/20190430012209_create_providers.rb
+++
@@ -1,9 +0,0 @@
-class CreateProviders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :providers do |t|
-      t.string :name
-      t.uid :name
-      t.references :user, foreign_key: true
-    end
-  end
-end
