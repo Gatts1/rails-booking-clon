a0fa2cbd2d8e0d126b694953658fa6cea3fd7e48
paulotijero <paulotijero@gmail.com>
2019-05-03 10:59:30 -0500

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "home#index"
   get '/search', to: 'home#search'
+  get '/promotions', to: 'home#promotions'
   
   namespace :admin do
     root to: "hotels#index"
