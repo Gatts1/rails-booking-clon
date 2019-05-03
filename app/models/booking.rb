0ba0a32ae0610a8205f4aa6782cfbb7963836e76
Marieth <mperezzevallos@gmail.com>
2019-05-03 10:14:41 -0500

    Fix model booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  after_create :notification_booking_created
 
   def self.available?(min_date, max_date)
     where('end_date >= ? AND start_date <= ?', min_date, max_date)
   end
-end
-  after_create :notification_booking_created
 
   def notification_booking_created
     BookingMailer.with(booking: self).booking_created_user.deliver_now
