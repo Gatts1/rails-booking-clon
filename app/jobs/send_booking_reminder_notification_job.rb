ff494cf15eb2a758782041d139990c5a04833b04
Marieth <mperezzevallos@gmail.com>
2019-05-03 18:59:25 -0500

    Delete trash and change deliver now for deliver_later

--- app/jobs/send_booking_reminder_notification_job.rb
+++ app/jobs/send_booking_reminder_notification_job.rb
@@ -3,15 +3,6 @@ class SendBookingReminderNotificationJob < ApplicationJob
 
   def perform(booking)
     # Do something later
-
-    # Booking.all.each do |booking|
-    #   if booking.start_date == DateTime.now+1.day
-    #     BookingMailer
-    #       .with(booking: booking)
-    #       .reminder_booking_user
-    #       .deliver_later
-    # end
-
     BookingMailer.with(booking: booking).reminder_booking_user.deliver_later
   end
 
