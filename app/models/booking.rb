b9f814043a43e57c6941fbcfc34b59eba261f704
Marieth <mperezzevallos@gmail.com>
2019-05-03 10:03:16 -0500

    Add booking created mailer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,3 +6,11 @@ class Booking < ApplicationRecord
     where('end_date >= ? AND start_date <= ?', min_date, max_date)
   end
 end
+  after_create :notification_booking_created
+
+  def notification_booking_created
+    BookingMailer.with(booking: self).booking_created_user.deliver_now
+    BookingMailer.with(booking: self).booking_created_hotel.deliver_now
+  end
+
+end
